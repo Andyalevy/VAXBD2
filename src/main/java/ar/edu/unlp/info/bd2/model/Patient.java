c0ca4a44c2c5a3672a0604e87bc2459bd34f9ddc
Alejo Pugliese <alejopugli@gmail.com>
2022-04-18 17:33:31 -0300

    ADD added Patient class

---
+++ src/main/java/ar/edu/unlp/info/bd2/model/Patient.java
@@ -0,0 +1,72 @@
+package ar.edu.unlp.info.bd2.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name="Patient")
+public class Patient {
+
+    @Id
+    @GeneratedValue
+    private Long Id;
+    private String email;
+    private String fullname;
+    private String password;
+    private Date dayOfBirth;
+
+    private VaccinationSchedule schedule;
+
+    public Patient() {
+    }
+
+    public Patient(String email, String fullname, String password, Date dayOfBirth) {
+        this.email = email;
+        this.fullname = fullname;
+        this.password = password;
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+    public void setDayOfBirth(Date dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public VaccinationSchedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(VaccinationSchedule schedule) {
+        this.schedule = schedule;
+    }
+}
