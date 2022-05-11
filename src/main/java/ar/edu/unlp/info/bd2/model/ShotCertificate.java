8421cee7c58851f2704c45a4801ac41c283df7af
Alejo Pugliese <alejopugli@gmail.com>
2022-05-11 20:29:11 -0300

    FIX readded shotcertificate

---
+++ src/main/java/ar/edu/unlp/info/bd2/model/ShotCertificate.java
@@ -0,0 +1,39 @@
+package ar.edu.unlp.info.bd2.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ShotCertificate")
+public class ShotCertificate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long serialNumber;
+    private Date date;
+
+    public ShotCertificate(){
+    }
+
+    public ShotCertificate(Date date){
+        this.setDate(date);
+    }
+
+    public void setDate(Date date){
+        this.date = date;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
+    public Long getSerialNumber() {
+        return serialNumber;
+    }
+
+}
